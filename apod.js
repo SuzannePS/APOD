ce1c71e088cac8c4c0c460934c6a80c9afaf791a
SuzannePS <suzannepatriciadesouza@yahoo.com.br>
2022-01-08 14:59:07 -0300

    ajuste

--- apod.js
+++ apod.js
@@ -2,23 +2,13 @@ let botao = $('#button')
 $('.video').addClass('none')
 
 botao.on('click', function(){
-    $('html').css('height' , 'auto')
-    $.ajax({url:'https://api.nasa.gov/planetary/apod?api_key=Uv1ybujgihcL6691DwmkOI09LI7ZZyHTXGit8h13'+date.value,     
-        success: function(result){         
+  let data = $("#data").val()
+    $.ajax({url:`//api.nasa.gov/planetary/apod?api_key=Uv1ybujgihcL6691DwmkOI09LI7ZZyHTXGit8h13&date=${data}`,     
+        success: function(result){ 
+        $(".mostra-img").css("display","flex")        
         $('.descricao').text(result.explanation)         
-        $('.autor').text(result.copyright)
-        
-        if(result.media_type=='image'){
-            $('.foto').attr('src', result.url)
-            $('.video').addClass('none')
-            $('.foto').removeClass('none')
-        }else{
-            $('.video').attr('src', result.url)
-            $('.video').removeClass('none')
-            $('.foto').addClass('none')
-        }
-   
-            }    
-         }) 
-     });
-apod()
+        $('.titulo').text(result.title)
+        $('#imagem').attr('src', result.url)
+        }   
+         })
+     })
